6f92694d8d67e75128e44e01192709ba01584076
naveena <100334873+21P31A04c4@users.noreply.github.com>
2022-02-24 10:56:58 +0530

    #include<stdio.h> int main() {     float celsius,farenhiet,n;   //  print("enter the temperature value");     scanf("%f",&farenhiet);     celsius=(farenhiet-32)*5/9;     printf("%.2f",celsius); }

---
+++ Fahrenheit_to_Celsius.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float celsius,farenhiet,n;
+  //  print("enter the temperature value");
+    scanf("%f",&farenhiet);
+    celsius=(farenhiet-32)*5/9;
+    printf("%.2f",celsius);
+}
